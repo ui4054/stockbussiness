183b671d363936366e6faa89f1b75371cdc079fc
MiguelMartinez <ui_4054@hotmail.com>
2021-09-10 19:35:12 -0500

    Update DB.js
    para tapar cositas

--- api/DB.js
+++ api/DB.js
@@ -2,6 +2,6 @@
 // DB.js
 
 module.exports = {
-    DB: 'mongodb+srv://adminstock:d1kyq1NBLzN9f73I@cluster0.9egxc.mongodb.net/Cluster0?retryWrites=true&w=majority'
+    DB: "ya tu querás saber"
     //DB : 'MONGODB_URI'
 }
