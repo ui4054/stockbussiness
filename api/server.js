7eabb48561cf5f3c7a43518d77901149778fd934
MiguelMartinez <ui_4054@hotmail.com>
2021-09-14 09:50:44 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -5,17 +5,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
+
+
 const mongoose = require('mongoose');
 const config = require('./DB.js');
 //const config = require(DB);
 const postRoute = require('./post.route');
-const uri = process.env.MONGODB_URI;
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => { console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)}
 );
+
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(() => { console.log('Database is connected') },
+  err => {
+  const collection = client.db("Cluster0").collection("post")
+  console.log('Can not connect to the database'+ err);
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,3 +40,11 @@ app.use('/posts', postRoute);
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
 });
+
+
+
+
+
+
+
+
