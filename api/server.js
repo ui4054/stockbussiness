ef1db190c0d17aa54eed580244f61a2e0bb4e513
MiguelMartinez <ui_4054@hotmail.com>
2021-09-14 11:00:36 -0500

    Servidores simulatoneos
    Front y back en servidores simulatenos

--- api/server.js
+++ api/server.js
@@ -5,31 +5,19 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
-
-
 const mongoose = require('mongoose');
 const config = require('./DB.js');
 //const config = require(DB);
 const postRoute = require('./post.route');
+//const uri = process.env.MONGODB_URI;
 
-
+// variables de entorno locales
+require('dotenv').config({ path: './variables.env'});
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
   () => { console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)}
 );
-
-const { MongoClient } = require('mongodb');
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(() => { console.log('Database is connected') },
-  err => {
-  const collection = client.db("Cluster0").collection("post")
-  console.log('Can not connect to the database'+ err);
-  // perform actions on the collection object
-  client.close();
-});
-
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -40,11 +28,3 @@ app.use('/posts', postRoute);
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
 });
-
-
-
-
-
-
-
-
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const uri = process.env.MONGODB_URI;
+
 const app = express()
 
 // agregamos el serve que continene el index.html
@@ -11,3 +11,41 @@ const port = process.env.PORT || 8080
 app.listen(port)
 
 console.log('listening on port: ' + port)
+
+
+require('dotenv').config({ path: './variables.env'});
+//const uri = process.env.MONGODB_URI;
+
+//const express2 = require('express');
+//const app2 = express2();
+//const bodyParser = require('body-parser');
+//const PORT2 = 4000;
+//const cors = require('cors');
+
+
+//const mongoose = require('mongoose');
+//const config = require('./DB.js');
+//const config = require(DB);
+//const postRoute = require('./api/post.route');
+
+
+//mongoose.Promise = global.Promise;
+//mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
+ // () => { console.log('Database is connected') },
+ // err => { console.log('Can not connect to the database'+ err)}
+//);
+
+//app.use(cors());
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
+
+//app.use('/posts', postRoute);
+
+//app.listen(PORT2, function(){
+//  console.log('Server is running on Port:',PORT2);
+//});
+
+
+
+
+
