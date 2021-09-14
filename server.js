a92da226ec5123f941d547e1e2645a1651199645
MiguelMartinez <ui_4054@hotmail.com>
2021-09-14 14:29:49 -0500

    Acatualización no mongo heroku

--- server.js
+++ server.js
@@ -16,34 +16,34 @@ console.log('listening on port: ' + port)
 
 
 
-const express2 = require('express');
-const app2 = express2();
-const bodyParser2 = require('body-parser');
-const PORT2 = 27126;
-const cors2 = require('cors');
-const mongoose = require('mongoose');
-const config = require('./DB.js');
+//const express2 = require('express');
+//const app2 = express2();
+//const bodyParser2 = require('body-parser');
+//const PORT2 = 4000;
+//const cors2 = require('cors');
+//const mongoose = require('mongoose');
+//const config = require('./DB.js');
 //const config = require(DB);
-const postRoute = require('./post.route');
+//const postRoute = require('./post.route');
 //const uri = process.env.MONGODB_URI;
 
 // variables de entorno locales
 //require('dotenv').config({ path: './variables.env'});
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
-  () => { console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
+//mongoose.Promise = global.Promise;
+//mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
+ // () => { console.log('Database is connected') },
+ // err => { console.log('Can not connect to the database'+ err)}
+//);
 
-app2.use(cors2());
-app2.use(bodyParser2.urlencoded({extended: true}));
-app2.use(bodyParser2.json());
+//app2.use(cors2());
+//app2.use(bodyParser2.urlencoded({extended: true}));
+//app2.use(bodyParser2.json());
 
-app2.use('/posts', postRoute);
+//app2.use('/posts', postRoute);
 
-app2.listen(PORT2, function(){
-  console.log('Server is running on Port:',PORT2);
-});
+//app2.listen(PORT2, function(){
+ // console.log('Server is running on Port:',PORT2);
+//});
 
 
 
