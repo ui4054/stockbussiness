2c17365cf74f292b32dbcf3ae3af9da51a84ff50
MiguelMartinez <ui_4054@hotmail.com>
2021-09-15 18:32:16 -0500

    heroku

--- src/components/CreateComponent.vue
+++ src/components/CreateComponent.vue
@@ -34,7 +34,7 @@
     },
     methods: {
       addPost(){
-        let uri = 'http://localhost:4000/posts/add';
+        let uri = 'https://stkbsprueba.herokuapp.com:4000/posts/add';
         this.axios.post(uri, this.post).then(() => {
           this.$router.push({name: 'posts'});
         });
--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -37,7 +37,7 @@
         }
       },
       created() {
-      let uri = 'http://localhost:4000/posts';
+      let uri = 'https://stkbsprueba.herokuapp.com/posts';
       this.axios.get(uri).then(response => {
         this.posts = response.data;
       });
@@ -45,7 +45,7 @@
     methods: {
       deletePost(id)
       {
-        let uri = `http://localhost:4000/posts/delete/${id}`;
+        let uri = `https://stkbsprueba.herokuapp.com:4000/posts/delete/${id}`;
         this.axios.delete(uri).then(response => {
           this.posts.splice(this.posts.indexOf(id), 1);
         });
