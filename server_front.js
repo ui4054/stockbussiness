7a06cb64b02449a19ee7b5ca7e4addc3b3befc54
MiguelMartinez <ui_4054@hotmail.com>
2021-09-15 10:59:18 -0500

    Restableciendo configuración original

---
+++ server_front.js
@@ -0,0 +1,41 @@
+// backend server
+//front end server
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+// agregamos el serve que continene el index.html
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
+
+
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log('listening on port: ' + port)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
