0a8e7905beebb3b7174bc09494685cbb2f4c3a36
Nancy4536 <nanytimana@gmail.com>
2021-10-05 21:53:11 -0500

    modificación About ver2
    cambio párrafo

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,8 @@
   <div class="about">
     <br><br>
 
-    <h1>StockBussiness es una solución web para pequeñas tiendas de barrio, en la que los clientes de los tenderos puedan realizar su pedido en línea y este puede ser recogido por el cliente o enviado, su pago es contraentrega.</h1>
+    <h1>StockBussiness </h1>
+    <p>es una solución web para pequeñas tiendas de barrio, en la que los clientes de los tenderos puedan realizar su pedido en línea y este puede ser recogido por el cliente o enviado, su pago es contraentrega.</p>
     <img src="" >
     <p></p>
   </div>
