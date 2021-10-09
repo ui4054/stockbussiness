2bea5cdf48c92aeee0a470e56d5e5910bbe84c73
Nancy4536 <nanytimana@gmail.com>
2021-10-09 16:56:18 -0500

    Modificación App
    cambio tipo de archivo en la imagen de logo

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="container-fluid">
         <a class="navbar-brand" href="/" id="navhome">
           <img
-            src="../src/assets/logotienda.jpg"
+            src="../src/assets/logotienda.png"
             alt=""
             height="65"
             class="d-inline-block align-text-top"
