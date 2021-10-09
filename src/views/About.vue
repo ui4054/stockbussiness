d5369ba857b097049c122086cc4ef89af2d0aba8
Nancy4536 <nanytimana@gmail.com>
2021-10-09 16:40:04 -0500

    Modificación en About
    cambio tamaño de logo Stockbusiness

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,13 @@
 <template>
-
   <div class="about">
-    <br><br>
+    <br /><br />
 
     <h1>StockBussiness</h1>
-    <p>Solución web para pequeñas tiendas de barrio, en la que los clientes de los tenderos pueden realizar su pedido en línea y este puede ser recogido por el cliente o enviado, su pago es contraentrega.</p>
-    <img src="../assets/logotienda.png" 
-    >
+    <p>
+      Solución web para pequeñas tiendas de barrio, en la que los clientes de
+      los tenderos pueden realizar su pedido en línea y este puede ser recogido
+      por el cliente o enviado, su pago es contraentrega.
+    </p>
+    <img src="../assets/logotienda.png" height="200" width="150" >
   </div>
 </template>
