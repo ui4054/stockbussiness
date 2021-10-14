d4bbc598008e6779292c38e712cb44f84145a5aa
MiguelMartinez <ui_4054@hotmail.com>
2021-10-13 21:38:40 -0500

    Update server_api.js

--- api/server_api.js
+++ api/server_api.js
@@ -3,16 +3,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 80;
+const PORT = 443;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./DB.js');
 //const config = require(DB);
 const postRoute = require('./post.route2');
-//const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 
 // variables de entorno locales - dptnev se inactiva en despliegue remoto
-require('dotenv').config({ path: './variables.env'});
+//require('dotenv').config({ path: './variables.env'});
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
   () => { console.log('Database is connected') },
